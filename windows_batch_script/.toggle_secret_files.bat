d89ea2f32fd11f2b84be579252abd1671d6faf1b
Shen <shen.h@outlook.com>
2021-03-27 22:34:37 +0800

    Finalize BAT and GIFs. Change flags to config.

--- windows_batch_script/.toggle_secret_files.bat
+++ windows_batch_script/.toggle_secret_files.bat
@@ -1,33 +1,31 @@
 :: Hide and Lock Files and Folders on Windows PC
 ::
-:: hss.wiki
+:: hss@hss.wiki
 ::
 :: Hiding files and folders in the same folder with this script by setting
-:: their system and hidden attributes. Those "secret files" (and folders)
+:: their System and Hidden attributes. Those "secret files" (and folders)
 :: cannot be easily shown in file explorer or by using `dir` in CMD.
 ::
-:: Basic usage: Put this BAT file in a folder that you want to hide its files
-:: and subfolders. Double click to run the script, and all others will be
-:: hidden. Refresh if needed to see the effect. To reveal those files and
-:: folders, simply double click the script again.
+:: Basic usage: Put this BAT file into the folder that you want to hide its
+:: files and subfolders. Double click the script to hide the files. Refresh if
+:: needed to see the effect. To reveal those files and folders, simply double
+:: click the script again.
 ::
 :: Intermediate usage: Set a passphrase under the "Config" section of this
-:: script. The files can be hid same as above, but revealing secret files will
-:: require the passphrase you just set.
+:: script, and it will be asked before revealing any secret files. The files
+:: can be hidden same as above.
 ::
-:: Advanced usage: Several flags can be specified in the filename of this
-:: script in order to lock this "key" script into a ZIP file. For example, if
-:: rename this script as `.toggle_secret_files -ppass -nname.zip -d.bat`, after
-:: hiding its neighbouring files and folders, the script will lock itself in a
-:: ZIP file called `name.zip` encrypted with password `pass`. The `-d` flag
-:: asks the script to delete itself after zipped, so that it works like moving
-:: to archive.
+:: Advanced usage: Set a EXE password under the "Config" section, and the
+:: script will be locked into a self-extracting EXE after hiding files and
+:: folders. To reveal the files, first, double click the EXE and enter the
+:: password to release the BAT script. Then, double click the script to reveal
+:: secret files. See the "Config" section for details.
 ::
 :: The installation of [7-Zip](https://www.7-zip.org/) (about 1.4 MB) is
-:: required if want to zip and encrypt the script. See also the "Program logic"
-:: section below for other advanced usage. While script passphrase can contain
-:: whitespaces, flags in the filename of this script cannot. All of them are
-:: case sensitive, but none of them supports special characters yet.
+:: required if want to adopt the advanced usage. While script passphrase, EXE
+:: filename, and password should work well with whitespaces, we advise you not
+:: to, since they haven't been thoroughly tested. All of them are case
+:: sensitive, but none of them supports special characters yet.
 
 :: ############################################################################
 ::   Program logic
@@ -35,19 +33,17 @@
 ::
 :: If the current folder contains any visible files/subfolders:
 ::     Hide them all as secret files/folders. Note that this script will not be
-::     hid.
+::     hidden.
 ::
 ::     If `exe_password` is configured:
 ::         Lock this script into an EXE file encrypted with the given password.
 ::
-::         If `exe_filename` is configured:
+::         If `exe_filename_w_extension` is configured:
 ::             Name the EXE file as given.
 ::
 :: Else:
-::     Reveal all secret files/folders in the current folder.
-::
-::     If `exe_remove_code` is configured:
-::         Remove the EXE file if possible.
+::     Reveal all secret files/folders in the current folder. Also, remove the
+::     EXE where this script extracts from if possible.
 ::
 ::     If `script_passphrase` is configured:
 ::         Request passphrase before revealing any secret files or folders.
@@ -83,7 +79,7 @@ set "exe_password="
 ::   Name the EXE file
 :: ============================================================================
 
-set "exe_filename=.toggle_secret_files.exe"
+set "exe_filename_w_extension=.toggle_secret_files.exe"
 
 :: By default, the EXE file is named as `.toggle_secret_files.exe`. Note that
 :: only when `exe_password` is defined, the BAT script will be locked into an
@@ -91,17 +87,7 @@ set "exe_filename=.toggle_secret_files.exe"
 :: EXE filename without password will not encrypt the BAT into EXE.
 
 :: ============================================================================
-::   Handle the EXE file after self-extracting
-:: ============================================================================
-
-:: TODO: update doc from here, also the header
-set "exe_remove_code=d"
-
-:: d to del, r to recycle,
-:: Toggle verbose mode
-
-:: ============================================================================
-::   Run the script in verbose mode
+::   Toggle verbose mode
 :: ============================================================================
 
 set "v="
@@ -134,23 +120,21 @@ cls
 ::   Validate configs
 :: ============================================================================
 
-if "%exe_remove_code%" == "d" (
-
-) else if "%exe_remove_code%" == "r" (
-
-)
-
 if defined v (
     if not defined exe_password (
         echo Script will not be encrypted since EXE password is not defined.
     ) else (
         echo EXE password: `%exe_password%`.
-        echo EXE filename: `%exe_filename%`.
+        echo EXE filename: `%exe_filename_w_extension%`.
     )
     echo.
 )
 :: `echo:` or `echo.` to echo a blank line.
 
+:: Remove residual EXE file if possible. It may exist after self-extracting
+:: to get this script.
+
+del "%exe_filename_w_extension%" 2> nul
 
 :: ============================================================================
 ::   Catalog current files and folders
@@ -161,6 +145,8 @@ if defined v (
 
 setlocal enableDelayedExpansion
 set "script_filename_w_extension=%~nx0"
+:: `%~n0` stores current file name, x for extension.
+:: Ref: https://stackoverflow.com/a/15568171. Substitute 1 with 0.
 
 :: Catalog secret files and folders
 set /a counter = 0
@@ -168,7 +154,7 @@ set /a n_secret_files = 0
 :: `/a` for numeric.
 
 for /f "tokens=1* delims=:" %%a in ('dir /ahs /b 2^>nul ^|findstr /n "^"') do (
-    if not "%%b" == "%script_filename_w_extension%" if not "%%b" == "%zip_n%" (
+    if not "%%b" == "%script_filename_w_extension%" (
         set /a counter += 1
         set "secret_file_number_!counter!=%%b"
         set "n_secret_files=!counter!"
@@ -176,8 +162,8 @@ for /f "tokens=1* delims=:" %%a in ('dir /ahs /b 2^>nul ^|findstr /n "^"') do (
 )
 :: `%%a` and `%%b` are local iterator vars.
 
-:: Excluding this script and its possible ZIP since we don't want to turn them
-:: into secret files. Uses double quotes since filename can contain spaces.
+:: Excluding this script since we don't want to hide it. Uses double quotes
+:: since filename can contain whitespaces.
 
 :: `dir /ahs /b 2^>nul ^|findstr /n "^"` is a command-line command to list and
 :: filter directories. `^` is used to escape special chars.
@@ -196,7 +182,7 @@ set /a counter = 0
 set /a n_files = 0
 
 for /f "tokens=1* delims=:" %%a in ('dir /a /b 2^> nul ^|findstr /n "^"') do (
-    if not "%%b" == "%script_filename_w_extension%" if not "%%b" == "%zip_n%" (
+    if not "%%b" == "%script_filename_w_extension%" (
         set /a counter += 1
         set "file_number_!counter!=%%b"
         set "n_files=!counter!"
@@ -207,6 +193,12 @@ if defined v (
     echo Here we have %n_secret_files% secret files/folders out of ^
 %n_files% files/folders in total.
     echo.
+)
+
+if %n_files% == 0 (
+    echo INFO: Current directory is empty.
+    echo.
+    goto terminate_and_display_error_message
 )
 
 :: ============================================================================
@@ -222,18 +214,17 @@ if %n_files% gtr %n_secret_files% (
 ) else (
     if defined v echo Will reveal secret files and folders.
 
-    if "%script_passphrase%" == "_none" (
-        @REM Double-quote in case passphrase contains whitespaces.
-        if defined v echo Script passphrase disabled.
-        goto reveal_secret_files
-    ) else (
-        if defined v echo Script passphrase required.
-
+    if defined script_passphrase (
+        if defined v (
+            echo Script passphrase required.
+            echo.
+        )
         :require_passphrase_to_reveal_files
         echo Enter passphrase to reveal secret files and folders:
         set /p "user_input_passphrase=>"
         @REM `>` will show in the console.
         if "!user_input_passphrase!" == "%script_passphrase%" (
+            @REM Double-quote in case passphrase contains whitespaces.
             @REM Note the use of delayed expansion here. Otherwise it won't
             @REM work in the first iteration even if the passphrase is correct.
             if defined v echo Passphrase match.
@@ -242,24 +233,30 @@ if %n_files% gtr %n_secret_files% (
             echo Passphrase does not match.
             goto require_passphrase_to_reveal_files
         )
+    ) else (
+        if defined v (
+            echo Script passphrase disabled.
+            echo.
+        )
+        goto reveal_secret_files
     )
 )
 
 :hide_all_files
-:: `:label` for goto statements to jump to.
 for /l %%n in (1 1 %n_files%) do (
-    attrib !file_number_%%n! +h +s
+    attrib "!file_number_%%n!" +h +s
+    @REM Note the double quotes in case filename contains whitespaces.
     if defined v echo Hid `!file_number_%%n!`.
 )
 if defined v (
     echo Successfully hid all %n_files% files and folders.
     echo.
 )
-goto self_encrypt_and_destruct_if_flagged
+goto self_encrypt_and_archive
 
 :reveal_secret_files
 for /l %%n in (1 1 %n_secret_files%) do (
-    attrib !secret_file_number_%%n! -h -s
+    attrib "!secret_file_number_%%n!" -h -s
     if defined v echo Revealed `!secret_file_number_%%n!`.
 )
 if defined v (
@@ -269,39 +266,38 @@ if defined v (
 goto end
 
 :: ============================================================================
-::   After hiding secret files/folders, self encrypt and destruct if flagged
+::   If applicable, self encrypt and archive after hiding secret files/folders
 :: ============================================================================
 
-:self_encrypt_and_destruct_if_flagged
-if defined zip_pw (
-    if defined d (
-        if defined v (
-            echo "MODE:D Self-Destruction Protocol Initiated. Sayonara."
-            echo.
-        )
-        7z a "%zip_n%" "%script_filename_w_extension%" -p"%zip_pw%" -sdel > nul
-        @REM `-sdel` to self delete files after compression.
-    ) else (
-        @REM Encrypt only. Leave one copy of the script outside.
-        7z a "%zip_n%" "%script_filename_w_extension%" -p"%zip_pw%" > nul
-    )
+:self_encrypt_and_archive
+if defined exe_password (
+    7z a -p"%exe_password%" -sdel -mhe=on ^
+-sfx "%exe_filename_w_extension%" "%script_filename_w_extension%" > nul
+
     if defined v (
-        echo Successfully locked the script into `%zip_n%` with password ^
-`%zip_pw%`.
+        echo Successfully locked the script into `%exe_filename_w_extension%` ^
+with password `%exe_password%`.
         echo.
     )
 )
+:: `-sdel` to Self DELete files after archiving, `-sfx` to create a Self
+:: EXtractable archive, `-mhe=on` to also encrypt archive HEaders (only works
+:: for the 7z format, just leave it here for reference).
 
 :: ============================================================================
 ::   End
 :: ============================================================================
+
+goto end
+:terminate_and_display_error_message
+cmd /k
 
 :end
 
 if defined v (
     cmd /k
 )
-:: Keep the CMD window open.
+:: Keep the CMD window open if verbose.
 
 :: ############################################################################
 ::   Coding notes
@@ -312,13 +308,14 @@ if defined v (
 :: - Move to PowerShell
 :: - Error handling. Keep the window open for error message.
 :: - Check if 7z is available from the command line. Catch errors.
-:: - Support special characters in zip password, filename, passphrase.
+:: - Support space and special chars in zip password, filename, passphrase.
 
 :: ============================================================================
 ::   Knowledge
 :: ============================================================================
 
 :: `^` for line continuation.
+:: `:label` for goto statements to jump to.
 
 :: Variable declaration
 
